1da2e1c35b67455c310a848afe797a02862a3322
AJSolis01 <AJSolis01@outlook.com>
2024-09-07 00:33:51 -0400

    PHP/Vue Personal Project

---
+++ Model/users.php
@@ -0,0 +1,29 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+
+require "database.php";
+
+if (!empty($_POST) && is_array($_POST)) {
+  $_POST = filter_var_array($_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+  $date_of_birth = "{$_POST['year']}-{$_POST['month']}-{$_POST['day']}"; 
+  $firstName     = $_POST['first_name'];
+  $lastName      = $_POST['last_name'];
+  $email         = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+
+  if (!$email || !ctype_alpha($firstName) || !ctype_alpha($lastName))
+  {
+    return false;
+  }
+
+  $insertSql = "INSERT INTO users (first_name, last_name, date_of_birth, email, timestamp) VALUES ('$firstName', '$lastName', '$date_of_birth', '$email', NOW())";
+
+  if ($conn->query($insertSql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $insertSql . "<br>" . $conn->error;
+  }
+}
+?>
---
+++ vue/src/components/navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-black bg-black">
+        <div class="container">
+            <!-- Centered brand/logo -->
+            <RouterLink class="navbar-brand mx-auto text-white" to="/">Home</RouterLink>
+
+            <!-- Toggler button for mobile -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Navbar links -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <RouterLink class="nav-link active text-white" to="/">Home</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link active text-white" to="/resume">Resume</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link active text-white" to="/signup">Register</RouterLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
